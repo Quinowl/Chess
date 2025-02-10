2d81f12e2ee180af52d78bce9bc48313370d24c0
Quinowl <127102187+Quinowl@users.noreply.github.com>
2025-02-10 13:17:44 +0100

    Completed board with pieces placement.

--- Chess/Assets/Core/Scripts/Managers/BoardManager.cs
+++ Chess/Assets/Core/Scripts/Managers/BoardManager.cs
@@ -25,16 +25,65 @@ public class BoardManager : MonoBehaviour
 
     private void PlacePieces()
     {
-        PlacePiece(Vector2Int.zero, EPieceType.Rook, EPieceColor.White);
-        PlacePiece(new Vector2Int(7, 0), EPieceType.Rook, EPieceColor.White);
+        PlacePawns(EPieceColor.White);
+        PlacePawns(EPieceColor.Black);
+        PlaceKnights();
+        PlaceBishops();
+        PlaceRooks();
+        PlaceQueens();
+        PlaceKings();
     }
 
     private void PlacePiece(Vector2Int position, EPieceType type, EPieceColor color)
     {
         Piece prefab = System.Array.Find(piecePrefabs, p => p.PieceType == type && p.PieceColor == color);
         if (!prefab) return;
-        Piece piece = Instantiate(prefab, new Vector3(position.x, 0f, position.y), Quaternion.identity);
+        Piece piece = Instantiate(prefab, new Vector3(position.x, 0f, position.y), Quaternion.identity, transform);
         pieces[position.x, position.y] = piece;
+    }
+
+    private void PlacePawns(EPieceColor pieceColor)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            PlacePiece(new Vector2Int(i, pieceColor == EPieceColor.White ? 1 : 6), EPieceType.Pawn, pieceColor);
+        }
+    }
+
+    private void PlaceRooks()
+    {
+        PlacePiece(Vector2Int.zero, EPieceType.Rook, EPieceColor.White);
+        PlacePiece(new Vector2Int(7, 0), EPieceType.Rook, EPieceColor.White);
+        PlacePiece(new Vector2Int(7, 7), EPieceType.Rook, EPieceColor.Black);
+        PlacePiece(new Vector2Int(0, 7), EPieceType.Rook, EPieceColor.Black);
+    }
+
+    private void PlaceKnights()
+    {
+        PlacePiece(new Vector2Int(1, 0), EPieceType.Knight, EPieceColor.White);
+        PlacePiece(new Vector2Int(6, 0), EPieceType.Knight, EPieceColor.White);
+        PlacePiece(new Vector2Int(6, 7), EPieceType.Knight, EPieceColor.Black);
+        PlacePiece(new Vector2Int(1, 7), EPieceType.Knight, EPieceColor.Black);
+    }
+
+    private void PlaceBishops()
+    {
+        PlacePiece(new Vector2Int(2, 0), EPieceType.Bishop, EPieceColor.White);
+        PlacePiece(new Vector2Int(5, 0), EPieceType.Bishop, EPieceColor.White);
+        PlacePiece(new Vector2Int(2, 7), EPieceType.Bishop, EPieceColor.Black);
+        PlacePiece(new Vector2Int(5, 7), EPieceType.Bishop, EPieceColor.Black);
+    }
+
+    private void PlaceKings()
+    {
+        PlacePiece(new Vector2Int(4, 0), EPieceType.King, EPieceColor.White);
+        PlacePiece(new Vector2Int(4, 7), EPieceType.King, EPieceColor.Black);
+    }
+
+    private void PlaceQueens()
+    {
+        PlacePiece(new Vector2Int(3, 0), EPieceType.Queen, EPieceColor.White);
+        PlacePiece(new Vector2Int(3, 7), EPieceType.Queen, EPieceColor.Black);
     }
 
     public void MovePiece(Vector2Int startPosition, Vector2Int targetPosition)
