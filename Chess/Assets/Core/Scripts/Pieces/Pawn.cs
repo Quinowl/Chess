6845710ce6083522933a58b6380649072467892f
Quinowl <127102187+Quinowl@users.noreply.github.com>
2025-02-10 13:52:03 +0100

    Started pawn behaviour

--- Chess/Assets/Core/Scripts/Pieces/Pawn.cs
+++ Chess/Assets/Core/Scripts/Pieces/Pawn.cs
@@ -4,6 +4,23 @@ public class Pawn : Piece
 {
     public override bool IsValidMove(Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
     {
+        int direction = pieceColor == EPieceColor.White ? 1 : -1;
+
+        if (targetPosition.x == startPosition.x && targetPosition.y == startPosition.y + direction && board[targetPosition.x, targetPosition.y] == null)
+            return true;
+
+        if (targetPosition.x == startPosition.x && targetPosition.y == startPosition.y + 2 * direction && board[targetPosition.x, targetPosition.y] == null && board[startPosition.x, startPosition.y + direction] == null)
+        {
+            if ((pieceColor == EPieceColor.White && startPosition.y == 1) || (pieceColor == EPieceColor.Black && startPosition.y == 6))
+                return true;
+        }
+
+        if (Mathf.Abs(targetPosition.x - startPosition.x) == 1 && targetPosition.y == startPosition.y + direction && board[targetPosition.x, targetPosition.y] != null)
+        {
+            if (board[targetPosition.x, targetPosition.y].PieceType != pieceType)
+                return true;
+        }
+
         return false;
     }
 }
