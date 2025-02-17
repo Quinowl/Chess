0e3ee17a29ef3c1e08e4b0675527cecf243f0a51
Quinowl <127102187+Quinowl@users.noreply.github.com>
2025-02-17 20:06:13 +0100

    King movement.

--- Chess/Assets/Core/Scripts/Pieces/King.cs
+++ Chess/Assets/Core/Scripts/Pieces/King.cs
@@ -4,6 +4,14 @@ public class King : Piece
 {
     public override bool IsValidMove(Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
     {
+        int deltaX = Mathf.Abs(targetPosition.x - startPosition.x);
+        int deltaY = Mathf.Abs(targetPosition.y - startPosition.y);
+
+        if (deltaX <= 1 && deltaY <= 1)
+        {
+            return board[targetPosition.x, targetPosition.y] == null || board[targetPosition.x, targetPosition.y].PieceType != pieceType;
+        }
+
         return false;
     }
 }
