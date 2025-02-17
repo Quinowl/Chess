e65ad646026be55c88e9899ec7bb00728b9183d7
Quinowl <127102187+Quinowl@users.noreply.github.com>
2025-02-17 19:57:01 +0100

    Refactored Queen with FunctionLibrary

--- Chess/Assets/Core/Scripts/Pieces/Queen.cs
+++ Chess/Assets/Core/Scripts/Pieces/Queen.cs
@@ -8,48 +8,10 @@ public class Queen : Piece
         int deltaY = targetPosition.y - startPosition.y;
 
         if (startPosition.x == targetPosition.x || startPosition.y == targetPosition.y)
-            return CheckStraightMove(startPosition, targetPosition, board);
+            return this.CheckStraightMove(startPosition, targetPosition, board);
         else if (Mathf.Abs(deltaX) == Mathf.Abs(deltaY))
-            return CheckDiagonalMove(startPosition, targetPosition, board);
+            return this.CheckDiagonalMove(startPosition, targetPosition, board);
 
         return false;
-    }
-
-    private bool CheckStraightMove(Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
-    {
-        int stepX = targetPosition.x > startPosition.x ? 1 : (targetPosition.x < startPosition.x ? -1 : 0);
-        int stepY = targetPosition.y > startPosition.y ? 1 : (targetPosition.y < startPosition.y ? -1 : 0);
-
-        int x = startPosition.x + stepX;
-        int y = startPosition.y + stepY;
-
-        while (x != targetPosition.x || y != targetPosition.y)
-        {
-            if (board[x, y] != null) return false;
-            x += stepX;
-            y += stepY;
-        }
-
-        return board[targetPosition.x, targetPosition.y] == null
-                    || board[targetPosition.x, targetPosition.y].PieceType != pieceType;
-    }
-
-    private bool CheckDiagonalMove(Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
-    {
-        int stepX = targetPosition.x > startPosition.x ? 1 : -1;
-        int stepY = targetPosition.y > startPosition.y ? 1 : -1;
-
-        int x = startPosition.x + stepX;
-        int y = startPosition.y + stepY;
-
-        while (x != targetPosition.x && y != targetPosition.y)
-        {
-            if (board[x, y] != null) return false;
-            x += stepX;
-            y += stepY;
-        }
-
-        return board[targetPosition.x, targetPosition.y] == null
-                || board[targetPosition.x, targetPosition.y].PieceType != pieceType;
     }
 }
--- Chess/Assets/Core/Scripts/Utils/FunctionLibrary.cs
+++ Chess/Assets/Core/Scripts/Utils/FunctionLibrary.cs
@@ -3,4 +3,42 @@ using UnityEngine;
 public static class FunctionLibrary
 {
     public static Vector2Int ToVector2Int(this Vector3 vector) => new Vector2Int(Mathf.RoundToInt(vector.x), Mathf.RoundToInt(vector.z));
+
+    public static bool CheckDiagonalMove(this Piece piece, Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
+    {
+        int stepX = targetPosition.x > startPosition.x ? 1 : -1;
+        int stepY = targetPosition.y > startPosition.y ? 1 : -1;
+
+        int x = startPosition.x + stepX;
+        int y = startPosition.y + stepY;
+
+        while (x != targetPosition.x && y != targetPosition.y)
+        {
+            if (board[x, y] != null) return false;
+            x += stepX;
+            y += stepY;
+        }
+
+        return board[targetPosition.x, targetPosition.y] == null
+                || board[targetPosition.x, targetPosition.y].PieceType != piece.PieceType;
+    }
+
+    public static bool CheckStraightMove(this Piece piece, Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
+    {
+        int stepX = targetPosition.x > startPosition.x ? 1 : (targetPosition.x < startPosition.x ? -1 : 0);
+        int stepY = targetPosition.y > startPosition.y ? 1 : (targetPosition.y < startPosition.y ? -1 : 0);
+
+        int x = startPosition.x + stepX;
+        int y = startPosition.y + stepY;
+
+        while (x != targetPosition.x || y != targetPosition.y)
+        {
+            if (board[x, y] != null) return false;
+            x += stepX;
+            y += stepY;
+        }
+
+        return board[targetPosition.x, targetPosition.y] == null
+                    || board[targetPosition.x, targetPosition.y].PieceType != piece.PieceType;
+    }
 }
