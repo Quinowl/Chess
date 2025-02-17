901fe69e48d7e174a8857dab5c3999efb59347ba
Quinowl <127102187+Quinowl@users.noreply.github.com>
2025-02-17 20:02:13 +0100

    Added bishop and rook movement.

--- Chess/Assets/Core/Scripts/Pieces/Bishop.cs
+++ Chess/Assets/Core/Scripts/Pieces/Bishop.cs
@@ -4,6 +4,13 @@ public class Bishop : Piece
 {
     public override bool IsValidMove(Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
     {
+        int deltaX = targetPosition.x - startPosition.x;
+        int deltaY = targetPosition.y - startPosition.y;
+        if (Mathf.Abs(deltaX) == Mathf.Abs(deltaY))
+        {
+            return this.CheckDiagonalMove(startPosition, targetPosition, board);
+        }
+
         return false;
     }
 }
--- Chess/Assets/Core/Scripts/Pieces/Rook.cs
+++ Chess/Assets/Core/Scripts/Pieces/Rook.cs
@@ -4,6 +4,8 @@ public class Rook : Piece
 {
     public override bool IsValidMove(Vector2Int startPosition, Vector2Int targetPosition, Piece[,] board)
     {
+        if (startPosition.x == targetPosition.x || startPosition.y == targetPosition.y)
+            return this.CheckStraightMove(startPosition, targetPosition, board);
         return false;
     }
 }
